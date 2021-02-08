ea1fb48adbefa22d5bea5955d3fe59d0db3f5ff8
zuca <2682zucazuca@gmail.com>
2021-02-08 16:10:55 +0900

    マイグレーションファイル作成

--- db/migrate/20210208055307_create_stages.rb
+++ db/migrate/20210208055307_create_stages.rb
@@ -1,7 +1,20 @@
 class CreateStages < ActiveRecord::Migration[6.0]
   def change
     create_table :stages do |t|
-
+      t.text :image
+      t.integer :category,     null: false
+      t.string :title,         null: false
+      t.string :group,         null: false
+      t.string :time,          null: false
+      t.date :start_day,       null: false
+      t.date :end_day,         null: false
+      t.string :theater,       null: false
+      t.integer :advance_price,null: false
+      t.integer :regular_price,null: false
+      t.string :writer
+      t.string :director
+      t.text :cast,            null: false
+      t.text :text
       t.timestamps
     end
   end
---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_08_055307) do
+
+  create_table "stages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "image"
+    t.integer "category", null: false
+    t.string "title", null: false
+    t.string "group", null: false
+    t.string "time", null: false
+    t.date "start_day", null: false
+    t.date "end_day", null: false
+    t.string "theater", null: false
+    t.integer "advance_price", null: false
+    t.integer "regular_price", null: false
+    t.string "writer"
+    t.string "director"
+    t.text "cast", null: false
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
